f5667bbbbba0acd3875d4c11b082479017541895
IsmoilSafarovFullStack <safarovismoil716@gmail.com>
2024-04-27 20:41:21 +0500

    first commit

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <Sidebar />
+</template>
+<script setup lang="ts">
+import Sidebar from '~/components/ui/Sidebar.vue';
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<Admin />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import Admin from '~/components/ui/Admin.vue';
+</script>
+
+<style></style>
